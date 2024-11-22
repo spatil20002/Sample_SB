bf6c0c526bc0d3aa80c62ce8326a16415bd86422
shubham <sp192555@gamail.com>
2024-11-22 10:03:31 +0530

    added even number

---
+++ src/pattern/EvenNumb.java
@@ -0,0 +1,14 @@
+package pattern;
+
+public class EvenNumb {
+	public static void main(String[] args) {
+		System.out.println("finding even number from 1 to 50");
+
+		for(int i=0;i<=50;i++) {
+			if(i%2==0) {
+				System.out.println(i+ " ");
+			}
+		}
+	}
+
+}
